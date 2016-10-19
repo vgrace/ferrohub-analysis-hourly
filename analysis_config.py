f80a187b3381e78208d8722366973ea7267a8f32
Vivian Esquivias <v.esquivias@gmail.com>
2016-10-19 10:16:23 +0200

    updated config

--- analysis_config.py
+++ analysis_config.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
-main_mongodb_uri = "mongodb://localhost:27017"
-main_mongodb = "test"
+#main_mongodb_uri = "mongodb://localhost:27017"
+#main_mongodb = "test"
+#local_mongodb_uri = "mongodb://localhost:27017"
+#local_mongodb = "test"
 
-local_mongodb_uri = "mongodb://localhost:27017"
-local_mongodb = "test"
+main_mongodb_uri = "mongodb://CNET:CnET_mongo_1_!@172.31.26.143:27017/ehubdata?authSource=ehubdata"
+main_mongodb = "ehubdata"
+local_mongodb_uri = "mongodb://127.0.0.1:27017"
+local_mongodb = "analytics"
